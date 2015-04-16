75526921a9d19b3cf0229bfa36722fc5d000de73
ppierce32118 <ppierce32118@gmail.com>
2015-04-16 18:14:21 -0400

    Lexical Scoping
    R function is able to cache potentially time-consuming
    computations

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## matrix value and inverse matrix value 
+## - add a matrix with makeCacheMatrix 
+## - get the inverse matrix with cacheSolve
+## - the inverse is only calculated the first time
 
-## Write a short comment describing this function
+
+## create a function to hold matrix value and inverse of matrix value
+## - sets matrix value 
+## - gets matrix value
+## - sets inverse of matrix value
+## - gets inverse of matrix value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inversematrix <- NULL
+        ## set the matrix value and initialize the inverse value
+        setmatrix <- function(newmatrix) {
+                x <<- newmatrix 
+                inversematrix <<- NULL
+        }
+        ## get the matrix value
+        getmatrix <- function() x
+        ## set the inverse matrix value
+        setinversematrix <- function(newinversematrix) {
+                inversematrix <<- newinversematrix
+        }
+        ## get the inverse matrix value
+        getinversematrix <- function() inversematrix
+        ## Return a list with functions for the matrix
+        list(setmatrix=setmatrix, 
+             getmatrix=getmatrix, 
+             setinversematrix=setinversematrix, 
+             getinversematrix=getinversematrix)
 }
 
 
-## Write a short comment describing this function
+## gets inverse matrix (stored in list created by function makeCacheMatrix)
+## from cache if not null or solve the matrix for inverse (and store)
 
 cacheSolve <- function(x, ...) {
+        ## gets inverse matrix value
+        inversematrix <- x$getinversematrix()
+        if (!is.null(inversematrix)) {
+                ## Return the cached inverse matrix if not null
+                return(inversematrix)
+        }
+        ## inverse matrix is null so solve the matrix for inverse value
+        inversematrix <- solve(x$getmatrix(), ...)
+        x$setinversematrix(inversematrix)
         ## Return a matrix that is the inverse of 'x'
+        inversematrix
 }
